de798b94c08441d99b6ed71437e29a3c49c4398d
Anatoliy Morozan <owopeef@gmail.com>
2024-11-15 11:11:23 +0300

    Added killed mob and rebith counts

--- src/main/java/ru/peef/mobannihilation/game/players/PlayerData.java
+++ src/main/java/ru/peef/mobannihilation/game/players/PlayerData.java
@@ -7,17 +7,21 @@ import java.util.List;
 public class PlayerData {
     public double level;
     public int gold;
+    public int mobKilled;
+    public int rebithCount;
     public List<RarityItem> rarity_items;
 
-    public PlayerData(int gold, double level, List<RarityItem> rarity_items) {
+    public PlayerData(int gold, int mobKilled, double level, int rebithCount, List<RarityItem> rarity_items) {
         this.gold = gold;
         this.level = level;
+        this.mobKilled = mobKilled;
+        this.rebithCount = rebithCount;
         this.rarity_items = rarity_items;
     }
 
     public int getLevel() { return (int) Math.floor(level); }
 
     public static PlayerData create(GamePlayer gamePlayer) {
-        return new PlayerData(gamePlayer.gold, gamePlayer.level, gamePlayer.getRarityItems());
+        return new PlayerData(gamePlayer.gold, gamePlayer.mobKilled, gamePlayer.level, gamePlayer.rebithCount, gamePlayer.getRarityItems());
     }
 }
